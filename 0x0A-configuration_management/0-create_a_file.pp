c69c8045d4f38bd16e3517a6a894c54d2cccca62
dozonalx <ukahdozzy@gmail.com>
2022-12-11 05:48:51 -0800

    fixing

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,7 +1,7 @@
 #Create a file
 file { '/tmp/school':
-	mode => '0744',
-	owner => 'www-data',
-	group => 'www-data',
-	content => 'I love puppet'
+mode    => '0744',
+owner   => 'www-data',
+group   => 'www-data',
+content => 'I love puppet'
 }
